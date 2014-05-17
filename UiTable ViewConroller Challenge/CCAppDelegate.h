9b2813f38132315ad1561de917be8573cf93167d
Yordan Georgiev <iordan340@gmail.com>
2014-05-17 16:15:48 +0300

    Initial Commit

---
+++ UiTable ViewConroller Challenge/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UiTable ViewConroller Challenge
+//
+//  Created by Yordan Georgiev on 5/17/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

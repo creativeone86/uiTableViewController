698ca69a495ddbea32ef049f6c72e1af69d14a9c
Yordan Georgiev <iordan340@gmail.com>
2014-05-17 16:39:13 +0300

    added table view

--- UiTable ViewConroller Challenge/CCTableViewController.h
+++ UiTable ViewConroller Challenge/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UiTable ViewConroller Challenge
 //
 //  Created by Yordan Georgiev on 5/17/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
